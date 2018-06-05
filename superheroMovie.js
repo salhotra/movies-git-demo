18bc01345f50496d8327d9a1a4d39d74cfbc7c8c
Nishant <nishantsalhotra.ns@gmail.com>
2018-06-05 20:42:38 +0530

    Feature: Add function to get favorite superhero movii

---
+++ superheroMovie.js
@@ -0,0 +1,10 @@
+function getFavoriteSuperheroMovie(person) {
+  switch (person) {
+    case 'Nishant':
+      return 'Captain America: Winter Soldier';
+    case 'Arfat':
+      return 'Avengers: Infinity War';
+    case 'Anshuman':
+      return 'Ramayan';
+  }
+}
